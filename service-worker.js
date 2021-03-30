3be9964b93bbce2b7fcd5161fcd0d6d5b27302a1
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-03-30 13:31:19 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.dc2827f0d70fdbbcf66d1e973a06962a.js"
 );
 
 self.addEventListener('message', (event) => {
